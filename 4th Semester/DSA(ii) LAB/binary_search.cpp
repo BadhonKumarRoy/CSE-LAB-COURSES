53fef22947762e6ec98b2c437211ae3339e287b8
BadhonKumarRoy <badhonroy957@gmail.com>
2026-05-11 00:46:59 +0600

    modified binarySearch

--- 4th Semester/DSA(ii) LAB/binary_search.cpp
+++ 4th Semester/DSA(ii) LAB/binary_search.cpp
@@ -1,23 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int binarySearch(int arr[], int l, int r, int x)
-{
-    int i = 1;
-    while(l<=r){
-        int mid = l + (r - l) / 2;
-        cout << "mid: " << mid << endl;
-        cout << "pass -------- " << i++ << endl << endl;
-        if(arr[mid] == x){
+int binarySearch(int arr[], int n, int key) {
+    int left = 0;
+    int right = n-1;
+
+    while(left <= right){
+        int mid = left + (right-left)/2;
+
+        if(arr[mid] ==  key)
             return mid;
-        }
-        else if(arr[mid] < x){
-            l = mid + 1;
-        }
-        else r = mid - 1;
+        else if(arr[mid] < key)
+            left = mid+1;
+        else if(arr[mid] > key)
+            right = mid-1;
     }
+
     return -1;
 }
+
 int main()
 {
     int n;
@@ -32,7 +33,7 @@ int main()
     int key;
     cout << "Enter the key to search: ";
     cin >> key;
-    int result = binarySearch(arr, 0, n - 1, key);
+    int result = binarySearch(arr, n, key);
     if(result == -1) {
         cout << "Element not found in the array." << endl;
     } else {
