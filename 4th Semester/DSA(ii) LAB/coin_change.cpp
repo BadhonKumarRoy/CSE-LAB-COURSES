cd3aaec1583aca183e632a2fa3783bbc72a6a072
BadhonKumarRoy <badhonroy957@gmail.com>
2026-05-24 04:44:40 +0600

    coin change

---
+++ 4th Semester/DSA(ii) LAB/coin_change.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n = 3;
+    int coins[n] = {2, 3, 5};
+    int amount = 100;
+
+    int dp[amount+1] = {0};
+    dp[0] = 1;
+
+    for(int i=0;i<n;i++){
+        int coin = coins[i];
+        for(int j=coin;j<=amount;j++){
+            dp[j] = dp[j] + dp[j-coin];
+        }
+    }
+
+    cout << dp[amount];
+}
