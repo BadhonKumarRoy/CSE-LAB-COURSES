b3fb50359cacb0adf8abd6fb8fde13eb25b0b58f
BadhonKumarRoy <badhonroy957@gmail.com>
2026-06-28 01:30:57 +0600

    created the project cpp file

---
+++ 4th Semester/DSA(ii) LAB/Project/Multi-Drone Rescue Algo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+#define c cin
+int main()
+{
+    int grid_size, drone, victim, obstacle;
+    cin >> grid_size;
+    cin >> drone;
+    cin >> victim;
+    c >> obstacle;
+    cout << "obstacle--" << obstacle << endl;
+}
